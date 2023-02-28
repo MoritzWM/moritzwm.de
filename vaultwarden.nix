0947172017d35fa92f0a8bbebc4ef20d0afc22cc
System administrator <root@v2202302194336219745.megasrv.de>
2023-02-28 18:03:08 +0100

    smtp for vaultwarden

--- vaultwarden.nix
+++ vaultwarden.nix
@@ -9,21 +9,21 @@
       WEB_VAULT_ENABLED = true;
       WEBSOCKET_ENABLED = true;
       SIGNUPS_VERIFY = true;
+      DOMAIN = "https://vault.twiessalla.de";
       DISABLE_ADMIN_TOKEN = false;
       SMTP_HOST = "posteo.de";
       SMTP_FROM = "cloud.twiessalla@posteo.de";
-      SMTP_FROM_NAME = "cloud.twiessalla_vaultwarden";
+      SMTP_FROM_NAME = "Vaultwarden";
       SMTP_PORT = 587;
       SMTP_SECURITY = "starttls";
-      SMTP_USERNAME = (import /var/vaultwarden_email.secret).SMTP_USERNAME;
-      SMTP_PASSWORD = (import /var/vaultwarden_email.secret).SMTP_PASSWORD;
+      SMTP_USERNAME = (import /var/vaultwarden_email.nix).SMTP_USERNAME;
+      SMTP_PASSWORD = (import /var/vaultwarden_email.nix).SMTP_PASSWORD;
       SMTP_TIMEOUT = 15;
       LOG_FILE = "/var/log/vaultwarden";
     };
   };
   security.acme.defaults.email = "tristan.wiessalla@posteo.de";
   security.acme.acceptTerms = true;
-
   services.nginx = {
     enable = true;
     
@@ -32,25 +32,40 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    virtualHosts = {
-      "vault.twiessalla.de" = {
-        forceSSL = true;
-        enableACME = true;
-        serverAliases = [ "vault.v2202302194336219745.megasrv.de" ];
-        locations."/" = {
-          proxyPass = "https://127.0.0.1:8800";
-          proxyWebsockets = true;
-        };
-        locations."/notifications/hub" = {
-          proxyPass = "https://127.0.0.1:3012";
-          proxyWebsockets = true;
-        };
-        locations."/notifications/hub/negotiate" = {
-          proxyPass = "https://127.0.0.1:8800";
-          proxyWebsockets = true;
-        };
 
+    virtualHosts."vault.twiessalla.de" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8000";
+	proxyWebsockets = true;
       };
-    };
+      locations."/notifications/hub" = {
+        proxyPass = "https://127.0.0.1:3012";
+        proxyWebsockets = true;
+      };
+      locations."/notifications/hub/negotiate" = {
+        proxyPass = "https://127.0.0.1:8800";
+        proxyWebsockets = true;
+      };
+     };
   };
+#  services.nginx = {
+#    enable = true;
+#    
+#    # Use recommended settings
+#    recommendedGzipSettings = true;
+#    virtualHosts = {
+#      "vault.twiessalla.de" = {
+#        forceSSL = true;
+#        enableACME = true;
+#        serverAliases = [ "vault.v2202302194336219745.megasrv.de" ];
+#        locations."/" = {
+#         proxyPass = "https://127.0.0.1:8800";
+#          proxyWebsockets = true;
+#       };
+#
+#     };
+#   };
+# };
 }

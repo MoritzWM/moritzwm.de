1822b4d0a0db433ff558e70c7be92ac0c3817286
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2023-07-15 12:50:07 +0200

    Limit network traffic

--- tor.nix
+++ tor.nix
@@ -10,6 +10,8 @@
       }
     ];
     settings.ContactInfo = "tor@moritzwm.de";
+    # I have 80 TB of traffic per month -> 30 MB/s -> 15 MB/s for send/receive + some buffer
+    settings.RelayBandwidthRate = "12 MBytes";
     openFirewall = true;
     relay.role = "relay";
   };

215ecffa349553b527c9452fc2d9a57b7424cb4c
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-01-28 15:37:00 +0100

    Remove deprecated grub option

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,6 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
   networking.hostName = "v2202205176338190863";

1bd5b53e00742117b365b377de5a5b16b2de25ee
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2025-05-21 21:23:04 +0200

    remove htpc, add mini

--- user-moritz.nix
+++ user-moritz.nix
@@ -6,7 +6,7 @@
     extraGroups = [ "wheel" "nextcloud" "paperless" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpKJNbeP/AReFpACmNIvfbpukdm2BwpnmOVszlxDVMj moritz@moritz-arch"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUI69bmgKa3TJC9tCTeB60X3dy4xgl3d5s7Ag3+0wq6 moritz@htpc"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBa68lr6VIyXXGL4XUgwTaPuH3hc7+3r8eTtkYH9ABac moritz@moritz-arch-mini"
     ];
   };
 }

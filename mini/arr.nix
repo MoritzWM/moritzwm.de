c47ab5b7df0e3685a78f896060846db26cbf0366
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2026-01-01 11:36:45 +0100

    set same group for all media handlers

--- mini/arr.nix
+++ mini/arr.nix
@@ -3,13 +3,16 @@
     services.sonarr = {
         enable = true;
         openFirewall = true;
+        group = "media";
     };
     services.radarr = {
         enable = true;
         openFirewall = true;
+        group = "media";
     };
     services.sabnzbd = {
         enable = true;
         openFirewall = true;
+        group = "media";
     };
 }
--- mini/jellyfin.nix
+++ mini/jellyfin.nix
@@ -1,7 +1,9 @@
 { config, pkgs, lib, sops-nix, ... }:
 {
+    users.groups.media.gid = 999;
     services.jellyfin = {
         enable = true;
         openFirewall = true;
+        group = "media";
     };
 }

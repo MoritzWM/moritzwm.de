bc7b27bb35ef479785a209502ee6eeba177f0a18
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2026-01-01 11:36:35 +0100

    switch to containerized deployment

--- mini/home-assistant.nix
+++ mini/home-assistant.nix
@@ -1,8 +1,16 @@
 { config, pkgs, lib, sops-nix, ... }:
 {
-    services.home-assistant = {
-        enable = true;
-        openFirewall = true;
-        config.homeassistant.time_zone = "Europe/Berlin";
-    };
+    networking.firewall.allowedTCPPorts = [ 8123 ];
+    virtualisation.oci-containers = {
+        backend = "podman";
+        containers.homeassistant = {
+            volumes = [ "home-assistant:/root/home-assistant-config" ];
+            environment.TZ = "Europe/Berlin";
+            image = "ghcr.io/home-assistant/home-assistant:stable";
+            extraOptions = [ 
+                "--network=host" 
+                # "--device=/dev/ttyACM0:/dev/ttyACM0"	# Example, change this to match your own hardware
+            ];
+        };
+	};
 }
